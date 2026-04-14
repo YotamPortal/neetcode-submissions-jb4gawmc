b15955ca01dc1eb470e9dacccb926a0fed9cdaf9
YotamPortal <44612484+YotamPortal@users.noreply.github.com>
2026-04-14 15:29:45 +0300

    Add: lru-cache - submission-4

---
+++ Data Structures & Algorithms/lru-cache/submission-4.cpp
@@ -0,0 +1,62 @@
+struct ListNode {
+    int key = 0;
+    int val = 0;
+    ListNode* next = nullptr;
+    ListNode* prev = nullptr;
+
+    ListNode(int _key, int _val, ListNode* _next = nullptr, ListNode* _prev = nullptr) :
+        key(_key), val(_val), next(_next), prev(_prev) {}
+};
+
+class LRUCache {
+    int cap;
+    std::unordered_map<int, ListNode*> keyToNode;
+    ListNode* head = nullptr;
+    ListNode* tail = nullptr;
+public:
+    LRUCache(int capacity) {
+        cap = capacity;
+        head = new ListNode(-1,-1);
+        tail = new ListNode(-1,-1);
+        head->next = tail;
+        tail->prev = head;    
+    }
+
+    void insert(int key, int value) {
+        ListNode* newNode = new ListNode(key, value);
+        keyToNode[key] = newNode;
+        newNode->next = head->next;
+        newNode->prev = head;
+        head->next->prev = newNode;
+        head->next = newNode;
+    }
+
+    void remove(ListNode* node) {
+        if (node) {
+            node->prev->next = node->next;
+            node->next->prev = node->prev;
+            keyToNode.erase(node->key);
+            delete node;
+        }
+    }
+    
+    int get(int key) {
+        int res = -1;
+        if (keyToNode.count(key)) {
+            res = keyToNode[key]->val;
+            remove(keyToNode[key]);
+            insert(key, res);
+        }
+        return res;
+    }
+    
+    void put(int key, int value) {
+        if (keyToNode.count(key)) {
+            remove(keyToNode[key]);    
+        }
+        insert(key, value);
+        if (keyToNode.size() > cap) {
+            remove(tail->prev);
+        }     
+    }
+};
