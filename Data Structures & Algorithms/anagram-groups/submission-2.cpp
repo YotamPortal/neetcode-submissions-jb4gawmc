ce9e5cc5b5a6aa269391f0c6480e412f92f79f54
YotamPortal <44612484+YotamPortal@users.noreply.github.com>
2026-05-16 23:57:24 +0300

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
+        if (strs.empty()) {
+            return res;
+        }
+        std::unordered_map<string, vector<string>> map;
+        for (auto str: strs) {
+            int count[26] = {0};
+            for (char c: str) {
+                count[c - 'a']++;
+            }
+            string key = "";
+            for (int i = 0; i < 26; i++) {
+                key += std::to_string(count[i]) + "#";
+            }
+            map[key].push_back(str);
+        }
+        for (auto list: map) {
+            res.push_back(list.second);
+        }
+        return res;
+    }
+};
