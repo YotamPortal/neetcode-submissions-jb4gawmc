edab950d690e777baec9475166f8f4f641b2b87d
YotamPortal <44612484+YotamPortal@users.noreply.github.com>
2026-05-16 23:17:22 +0300

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++) {
+            int missingNum = target - nums[i];
+            if (map.find(missingNum) != map.end()) {
+                return {map[missingNum], i};
+            }
+            map[nums[i]] = i;
+        }
+        return {};    
+    }
+};
